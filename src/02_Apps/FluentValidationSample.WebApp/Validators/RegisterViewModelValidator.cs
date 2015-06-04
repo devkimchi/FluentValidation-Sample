f81afe0bdf955470c12f3c8db0163661d564b42e
Justin Yoo <justin.yoo@aliencube.com>
2015-06-04 23:03:45 +1000

    Initial source code commit

---
+++ src/02_Apps/FluentValidationSample.WebApp/Validators/RegisterViewModelValidator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FluentValidation;
+using FluentValidationSample.WebApp.Models;
+
+namespace FluentValidationSample.WebApp.Validators
+{
+    public class RegisterViewModelValidator : AbstractValidator<RegisterViewModel2>
+    {
+        public RegisterViewModelValidator()
+        {
+            RuleFor(x => x.Email).NotNull();
+            RuleFor(x => x.Password).NotNull().Length(6, 100);
+            RuleFor(x => x.ConfirmPassword).NotNull().Equal(x => x.Password);
+        }
+    }
+}
